4bd7c52c0927f2488c91b642edef336dcbe30fcc
Tamas Kalman <tamas@mobileplusinc.com>
2012-07-03 15:30:05 -0700

    Code reformatted
    Refactored, PHP4 Support dropped

--- timer_example.php
+++ timer_example.php
@@ -1,32 +1,38 @@
 #!/usr/local/bin/php
-<?
+<?php
 
-// --- Timer.class example script
-// --- author: dh at squidcode dot com
-// --- web: http://dh.squidcode.com
+/**
+ * Timer.class example script
+ * 
+ * @author ktamas77 at gmail dot com
+ */
 
-error_reporting (E_ALL);
+error_reporting(E_ALL);
 
-require_once ('timer.class.inc.php');
+require_once 'timer.class.inc.php';
 
 $timer = new Timer();
 
 $a = 0;
 
 $timer->start('cycle');
-for ($i=0;$i<100000;$i++) $a *= $i;
+for ($i = 0; $i < 100000; $i++) {
+    $a *= $i;
+}
 $timer->stop('cycle');
 
-$timer->start ('date');
-for ($i=0;$i<10000;$i++) $d = date ("Y-m-d H:i");
-$timer->stop ('date');
+$timer->start('date');
+for ($i = 0; $i < 10000; $i++) {
+    $d = date("Y-m-d H:i");
+}
+$timer->stop('date');
 
-for ($i=0;$i<10;$i++) {
-	$timer->start ("subloop");
-	for ($j=0;$j<1000000;$j++) $a = $i*$j;
-	$timer->stop ("subloop");
+for ($i = 0; $i < 10; $i++) {
+    $timer->start("subloop");
+    for ($j = 0; $j < 1000000; $j++) $a = $i * $j;
+    $timer->stop("subloop");
 }
 
-var_dump ($timer->getAll());
+var_dump($timer->getAll());
 
 ?>
